f4c272e58381635524082c0000ee89b809fc4832
mori0711 <mori-kazuki-mp@ynu.jp>
2016-10-08 22:23:02 +0900

    check the number of CPU in Mac OS.

--- src/burai/com/env/MacCPUInfo.java
+++ src/burai/com/env/MacCPUInfo.java
@@ -9,7 +9,15 @@
 
 package burai.com.env;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class MacCPUInfo extends CPUInfo {
+
+    private static final String CHK_MAC_CPUCOM = "system_profiler SPHardwareDataType";
+
+    private static final String CPU_WORD = "Cores";
 
     public MacCPUInfo() {
         super();
@@ -22,6 +30,32 @@ public class MacCPUInfo extends CPUInfo {
          *  TODO: get #cpus for MacOS.
          */
 
-        return 1;
+        int numCPUs = 0;
+
+        try {
+            Process process = Runtime.getRuntime().exec(CHK_MAC_CPUCOM);
+            BufferedReader in = new BufferedReader(new InputStreamReader(process
+                .getInputStream()));
+
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                line = line.trim();
+                if (line.contains(CPU_WORD)){
+                    String[] nCPULine = line.split(" ", 0);
+                    if (nCPULine.length == 0){
+                        break;
+                    }
+                    else {
+                        numCPUs = Integer.parseInt(nCPULine[nCPULine.length -1]);
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            // TODO
+            e.printStackTrace();
+        }
+
+        return numCPUs;
     }
 }
